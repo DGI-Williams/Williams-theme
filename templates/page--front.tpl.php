0d86cce6bd8e6abff1345842b2d09a3d14067f13
Jamie <james@discoverygarden.ca>
2013-04-17 15:30:58 -0300

    End of Wed

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -119,8 +119,7 @@
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($tabs); ?>
-        <?php print render($title_prefix); ?>
+      <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1 class="title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
